c6836796ef2732e60d786b4a318091a1bbe0c38e
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-07-08 17:45:11 +0200

    total number of infos added

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
     req.addEventListener('load', function() {
         //on choisit une ligne au hasard dans la reponse
         var lines = req.responseText.split('\n');
+        //on veut savoir le nombre d'infos
+        var nbLines = lines.length;
+        document.querySelector('.nbLines').innerHTML = "<div class='fin'><div class='container'>La base de données comporte actuellement "+nbLines+" informations inutiles.</div></div>";
+        //on choisit une ligne au hasard
         var line = lines[Math.floor(Math.random() * lines.length)];
         //on affiche la ligne dans la page
         document.querySelector('.content').innerHTML = "<div class='container'><div class='info'>"+line+"</div></div>"; 
