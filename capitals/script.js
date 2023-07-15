bfcbac4391de8a46ba3736db48eea2b76a260b9e
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-07-16 00:38:06 +0200

    Capitals is now fully operational

--- capitals/script.js
+++ capitals/script.js
@@ -1,21 +1,31 @@
 var score = 0;
-var vie = 3;
+var vie = 4;
 
-var pays = ["France", "Allemagne", "Italie", "Espagne", "Portugal", "Belgique", "Angleterre", "Suisse", "Autriche", "Pays-Bas (économique)", "Pays-Bas (administrative))"];
-var capitales = ["Paris", "Berlin", "Rome", "Madrid", "Lisbonne", "Bruxelles", "Londres", "Berne", "Vienne", "Amsterdam", "La Haye"];
+var pays = ["Albanie", "Allemagne", "Andorre", "Arménie", "Autriche", "Azerbaïdjan", "Belgique", "Biélorussie", "Bosnie-Herzégovine", "Bulgarie", "Chypre", "Croatie", "Danemark", "Espagne", "Estonie", "Finlande", "France", "Géorgie", "Grèce", "Hongrie", "Irlande", "Islande", "Italie", "Kosovo", "Lettonie", "Liechtenstein", "Lituanie", "Luxembourg", "Macédoine du Nord", "Malte", "Moldavie", "Monaco", "Monténégro", "Norvège", "Pays-Bas", "Pologne", "Portugal", "République tchèque", "Roumanie", "Royaume-Uni", "Russie", "Saint-Marin", "Serbie", "Slovaquie", "Slovénie", "Suède", "Suisse", "Turquie", "Ukraine", "Vatican"];
+var capitales = ["Tirana", "Berlin", "Andorre-la-Vieille", "Erevan", "Vienne", "Bakou", "Bruxelles", "Minsk", "Sarajevo", "Sofia", "Nicosie", "Zagreb", "Copenhague", "Madrid", "Tallinn", "Helsinki", "Paris", "Tbilissi", "Athènes", "Budapest", "Dublin", "Reykjavik", "Rome", "Pristina", "Riga", "Vaduz", "Vilnius", "Luxembourg", "Skopje", "La Valette", "Chisinau", "Monaco", "Podgorica", "Oslo", "Amsterdam", "Varsovie", "Lisbonne", "Prague", "Bucarest", "Londres", "Moscou", "Saint-Marin", "Belgrade", "Bratislava", "Ljubljana", "Stockholm", "Berne", "Ankara", "Kiev", "Vatican"];
 
 function jeu(){
-    while (vie > 0){
-        var i = Math.floor(Math.random() * pays.length);
-        var capitale = prompt("Quelle est la capitale de " + pays[i] + " ?");
+    var i = Math.floor(Math.random() * pays.length);
+    var capitale = prompt("Quelle est la capitale de ce pays : " + pays[i] + " ?");
 
-        if (capitale == capitales[i]) {
-            score++;
-        } else {
-            vie--;
-            document.querySelector(".answer").innerHTML = "La bonne réponse était " + capitales[i];
-        }
-        document.querySelector(".score").innerHTML = "Score : " + score;
+    if (capitale == capitales[i]) {
+        score++;
+        document.querySelector(".answer").innerHTML = "Bonne réponse !";
+        document.querySelector(".answer").style.backgroundColor = "#00ff00";
+    } else {
+        vie--;
+        document.querySelector(".answer").innerHTML = "La bonne réponse était " + capitales[i];
+        document.querySelector(".answer").style.backgroundColor = "#ff0000";
     }
-    document.querySelector(".buttoncontainer").innerHTML = "<button onclick='location.reload()'>Rejouer</button>";
+    document.querySelector(".score").innerHTML = "Score : " + score;
+    afficheVie();
+
+    if (vie < 1) {
+        document.querySelector(".answer").innerHTML = "Vous avez perdu !";
+        document.querySelector(".buttoncontainer").innerHTML = "<button onclick='location.reload()' class='learn-more'><span class='circle' aria-hidden='true'><span class='icon arrow'></span></span><span class='button-text'>Rejouer</span></button>";
+    }
+}
+
+function afficheVie(){
+    document.querySelector(".vie").innerHTML = "<img src='assets/pictures/heart-"+vie+".png' alt='vie "+vie+"' >";
 }
