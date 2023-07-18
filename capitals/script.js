2c6a089f629ec966c67ceeda5493a36e2b4ba0c5
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-07-18 23:53:33 +0200

    Little fixes

--- capitals/script.js
+++ capitals/script.js
@@ -1,9 +1,6 @@
 var score = 0;
 var vie = 4;
 
-var pays = ["Albanie", "Allemagne", "Andorre", "Arménie", "Autriche", "Azerbaïdjan", "Belgique", "Biélorussie", "Bosnie-Herzégovine", "Bulgarie", "Chypre", "Croatie", "Danemark", "Espagne", "Estonie", "Finlande", "France", "Géorgie", "Grèce", "Hongrie", "Irlande", "Islande", "Italie", "Kosovo", "Lettonie", "Liechtenstein", "Lituanie", "Luxembourg", "Macédoine du Nord", "Malte", "Moldavie", "Monaco", "Monténégro", "Norvège", "Pays-Bas", "Pologne", "Portugal", "République tchèque", "Roumanie", "Royaume-Uni", "Russie", "Saint-Marin", "Serbie", "Slovaquie", "Slovénie", "Suède", "Suisse", "Turquie", "Ukraine", "Vatican"];
-var capitales = ["Tirana", "Berlin", "Andorre-la-Vieille", "Erevan", "Vienne", "Bakou", "Bruxelles", "Minsk", "Sarajevo", "Sofia", "Nicosie", "Zagreb", "Copenhague", "Madrid", "Tallinn", "Helsinki", "Paris", "Tbilissi", "Athènes", "Budapest", "Dublin", "Reykjavik", "Rome", "Pristina", "Riga", "Vaduz", "Vilnius", "Luxembourg", "Skopje", "La Valette", "Chisinau", "Monaco", "Podgorica", "Oslo", "Amsterdam", "Varsovie", "Lisbonne", "Prague", "Bucarest", "Londres", "Moscou", "Saint-Marin", "Belgrade", "Bratislava", "Ljubljana", "Stockholm", "Berne", "Ankara", "Kiev", "Vatican"];
-
 function jeu(){
     let i = Math.floor(Math.random() * pays.length);
     let capitale = prompt("Quelle est la capitale de ce pays : " + pays[i] + " ?");
@@ -38,9 +35,15 @@ function jeu(){
         document.querySelector(".answer").innerHTML = "Vous avez Gagné !";
         document.querySelector(".answer").style.backgroundColor = "#FFD700";
         document.querySelector(".buttoncontainer").innerHTML = "<button onclick='location.reload()' class='learn-more'><span class='circle' aria-hidden='true'><span class='icon arrow'></span></span><span class='button-text'>Rejouer</span></button>";
-        document.body.style.backgroundImage = "url('assets/pictures/rickroll.gif')";
+        document.body.style.backgroundImage = "url('assets/pictures/rickroll.webp')";
     }
 }
+
+document;addEventListener("keydown", function (e) {
+    if (e.key == "Enter") {
+        jeu();
+    }
+});
 
 function afficheVie(){
     document.querySelector(".vie").innerHTML = "<img src='assets/pictures/heart-"+vie+".png' alt='vie : "+vie+"' >";
