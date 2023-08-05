0e29dcd627e9b85fc1ca3ebc5e6739dfb7be27b9
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-08-05 17:10:45 +0200

    bug fixes, clues added to help the player, displaying the first letter of the capital city

--- capitals/script.js
+++ capitals/script.js
@@ -1,11 +1,19 @@
 var score = 0;
 var vie = 4;
+var clue = false;
+var audioright = document.createElement("audio");
+audioright.src = "assets/audio/valid.mp3";
+var audiowrong = document.createElement("audio");
+audiowrong.src = "assets/audio/wrong.mp3";
+var rickroll = document.createElement("audio");
+rickroll.src = "assets/audio/rickroll.mp3";
 
 function jeu(){
     i = Math.floor(Math.random() * pays.length);
     document.querySelector(".answer").innerHTML = "Quelle est la capitale de " + pays[i] + " ?";
     document.querySelector(".answer").style.backgroundColor = "transparent";
     document.querySelector(".buttoncontainer").style.display = "none";
+    document.querySelector(".indice").style.display = "flex";
     document.querySelector(".reponse").style.display = "flex";
     document.querySelector(".reponse").style.flexDirection = "column";
     document.querySelector(".reponse").innerHTML = "<input type='text' id='reponse' placeholder='Réponse' autofocus><br><button onclick='checkans()' class='learn-more' id='valid'><span class='circle' aria-hidden='true'><span class='icon arrow'></span></span><span class='button-text'>Valider</span></button>";
@@ -18,18 +26,35 @@ function checkans(){
 
     if (capitale == null) {
         document.querySelector(".answer").innerHTML = "Vous avez abandonné !";
+        document.querySelector(".indice").style.display = "none";
         document.querySelector(".buttoncontainer").innerHTML = "<button onclick='location.reload()' class='learn-more'><span class='circle' aria-hidden='true'><span class='icon arrow'></span></span><span class='button-text'>Rejouer</span></button>";
         return;
     } else
     if (capitale == capitales[i] || capitale.toLowerCase() == capitales[i].toLowerCase() || capitale.toUpperCase() == capitales[i].toUpperCase() || (capitale == "andorre la vieille" && capitales[i] == "Andorre-la-Vieille")) {
-        score++;
+        if (clue == true) {
+            score += 0.5;
+            clue = false;
+        }
+        else{
+            score++;
+        }
         document.querySelector(".answer").innerHTML = "Bonne réponse !";
         document.querySelector(".answer").style.backgroundColor = "#00ff00";
+        audioright.play();
+        document.querySelector(".indice").style.display = "none";
         var x = setTimeout(jeu, 1000);
     } else {
         vie--;
+        if (vie < 1) {
+            document.querySelector(".answer").innerHTML = "Vous avez perdu !";
+            document.querySelector(".reponse").innerHTML = "<button onclick='location.reload()' class='learn-more'><span class='circle' aria-hidden='true'><span class='icon arrow'></span></span><span class='button-text'>Rejouer</span></button>";
+            document.querySelector(".indice").style.display = "none";
+            return;
+        }
         document.querySelector(".answer").innerHTML = "La bonne réponse était " + capitales[i];
         document.querySelector(".answer").style.backgroundColor = "#ff0000";
+        document.querySelector(".indice").style.display = "none";
+        audiowrong.play();
         var x = setTimeout(jeu, 2000);
     }
     //on veut enlever le pays et la capitale de la liste
@@ -45,17 +70,12 @@ function checkans(){
     }
     afficheVie();
 
-    if (vie < 1) {
-        document.querySelector(".answer").innerHTML = "Vous avez perdu !";
-        document.querySelector(".reponse").innerHTML = "<button onclick='location.reload()' class='learn-more'><span class='circle' aria-hidden='true'><span class='icon arrow'></span></span><span class='button-text'>Rejouer</span></button>";
-        clearTimeout(x);
-        return;
-    }
     if (pays.length < 1) {
         document.querySelector(".answer").innerHTML = "Vous avez Gagné !";
         document.querySelector(".answer").style.backgroundColor = "#FFD700";
         document.querySelector(".reponse").innerHTML = "<button onclick='location.reload()' class='learn-more'><span class='circle' aria-hidden='true'><span class='icon arrow'></span></span><span class='button-text'>Rejouer</span></button>";
         document.body.style.backgroundImage = "url('assets/pictures/rickroll.webp')";
+        rickroll.play();
         document.querySelector(".answerspace").style.backgroundColor = "transparent";
         pays.length = 0;
         clearTimeout(x);
@@ -70,7 +90,7 @@ function checkans(){
 }
 
 
-
+var
 document;addEventListener("keydown", function (e) {
     if (e.key == "Enter" && pays.length > 0 && vie > 0) {
         checkans();
@@ -82,28 +102,28 @@ function afficheVie(){
 }
 
 function asie(){
-    var pays = ["Afghanistan", "Arabie Saoudite", "Arménie", "Azerbaïdjan", "Bahreïn", "Bangladesh", "Bhoutan", "Birmanie", "Brunei", "Cambodge", "Chine", "Corée du Nord", "Corée du Sud", "Émirats arabes unis", "Géorgie", "Inde", "Indonésie", "Irak", "Iran", "Israël", "Japon", "Jordanie", "Kazakhstan", "Kirghizistan", "Koweït", "Laos", "Liban", "Malaisie", "Maldives", "Mongolie", "Népal", "Oman", "Ouzbékistan", "Pakistan", "Philippines", "Qatar", "Russie", "Singapour", "Sri Lanka", "Syrie", "Tadjikistan", "Taïwan", "Thaïlande", "Timor oriental", "Turkménistan", "Turquie", "Viêt Nam", "Yémen"];
-    var capitales = ["Kaboul", "Riyad", "Erevan", "Bakou", "Manama", "Dacca", "Thimphou", "Naypyidaw", "Bandar Seri Begawan", "Phnom Penh", "Pékin", "Pyongyang", "Séoul", "Abou Dabi", "Tbilissi", "New Delhi", "Jakarta", "Bagdad", "Téhéran", "Jérusalem", "Tokyo", "Amman", "Noursoultan", "Bichkek", "Koweït", "Vientiane", "Beyrouth", "Kuala Lumpur", "Malé", "Oulan-Bator", "Katmandou", "Mascate", "Tachkent", "Islamabad", "Manille", "Doha", "Moscou", "Singapour", "Colombo", "Damas", "Douchanbé", "Taipei", "Bangkok", "Dili", "Achgabat", "Ankara", "Hanoï", "Sanaa"];
+    pays = ["Afghanistan", "Arabie Saoudite", "Arménie", "Azerbaïdjan", "Bahreïn", "Bangladesh", "Bhoutan", "Birmanie", "Brunei", "Cambodge", "Chine", "Corée du Nord", "Corée du Sud", "Émirats arabes unis", "Géorgie", "Inde", "Indonésie", "Irak", "Iran", "Israël", "Japon", "Jordanie", "Kazakhstan", "Kirghizistan", "Koweït", "Laos", "Liban", "Malaisie", "Maldives", "Mongolie", "Népal", "Oman", "Ouzbékistan", "Pakistan", "Philippines", "Qatar", "Russie", "Singapour", "Sri Lanka", "Syrie", "Tadjikistan", "Taïwan", "Thaïlande", "Timor oriental", "Turkménistan", "Turquie", "Viêt Nam", "Yémen"];
+    capitales = ["Kaboul", "Riyad", "Erevan", "Bakou", "Manama", "Dacca", "Thimphou", "Naypyidaw", "Bandar Seri Begawan", "Phnom Penh", "Pékin", "Pyongyang", "Séoul", "Abou Dabi", "Tbilissi", "New Delhi", "Jakarta", "Bagdad", "Téhéran", "Jérusalem", "Tokyo", "Amman", "Noursoultan", "Bichkek", "Koweït", "Vientiane", "Beyrouth", "Kuala Lumpur", "Malé", "Oulan-Bator", "Katmandou", "Mascate", "Tachkent", "Islamabad", "Manille", "Doha", "Moscou", "Singapour", "Colombo", "Damas", "Douchanbé", "Taipei", "Bangkok", "Dili", "Achgabat", "Ankara", "Hanoï", "Sanaa"];
     firstgame("Asie");
 }
 function afrique(){
-    var pays = ["Afrique du Sud", "Algérie", "Angola", "Bénin", "Botswana", "Burkina Faso", "Burundi", "Cameroun", "Cap-Vert", "République centrafricaine", "Comores", "République du Congo", "République démocratique du Congo", "Côte d'Ivoire", "Djibouti", "Égypte", "Érythrée", "Éthiopie", "Gabon", "Gambie", "Ghana", "Guinée", "Guinée-Bissau", "Guinée équatoriale", "Kenya", "Lesotho", "Liberia", "Libye", "Madagascar", "Malawi", "Mali", "Maroc", "Maurice", "Mauritanie", "Mozambique", "Namibie", "Niger", "Nigeria", "Ouganda", "Rwanda", "Sao Tomé-et-Principe", "Sénégal", "Seychelles", "Sierra Leone", "Somalie", "Soudan", "Soudan du Sud", "Swaziland", "Tanzanie", "Tchad", "Togo", "Tunisie", "Zambie", "Zimbabwe"];
-    var capitales = ["Pretoria", "Alger", "Luanda", "Porto-Novo", "Gaborone", "Ouagadougou", "Bujumbura", "Yaoundé", "Praia", "Bangui", "Moroni", "Brazzaville", "Kinshasa", "Yamoussoukro", "Djibouti", "Le Caire", "Asmara", "Addis-Abeba", "Libreville", "Banjul", "Accra", "Conakry", "Bissau", "Malabo", "Nairobi", "Maseru", "Monrovia", "Tripoli", "Antananarivo", "Lilongwe", "Bamako", "Rabat", "Port-Louis", "Nouakchott", "Maputo", "Windhoek", "Niamey", "Abidjan", "Kampala", "Kigali", "São Tomé", "Dakar", "Victoria", "Freetown", "Mogadiscio", "Khartoum", "Djouba", "Mbabane", "Dodoma", "N'Djaména", "Lomé", "Tunis", "Lusaka", "Harare"];   
+    pays = ["Afrique du Sud", "Algérie", "Angola", "Bénin", "Botswana", "Burkina Faso", "Burundi", "Cameroun", "Cap-Vert", "République centrafricaine", "Comores", "République du Congo", "République démocratique du Congo", "Côte d'Ivoire", "Djibouti", "Égypte", "Érythrée", "Éthiopie", "Gabon", "Gambie", "Ghana", "Guinée", "Guinée-Bissau", "Guinée équatoriale", "Kenya", "Lesotho", "Liberia", "Libye", "Madagascar", "Malawi", "Mali", "Maroc", "Maurice", "Mauritanie", "Mozambique", "Namibie", "Niger", "Nigeria", "Ouganda", "Rwanda", "Sao Tomé-et-Principe", "Sénégal", "Seychelles", "Sierra Leone", "Somalie", "Soudan", "Soudan du Sud", "Swaziland", "Tanzanie", "Tchad", "Togo", "Tunisie", "Zambie", "Zimbabwe"];
+    capitales = ["Pretoria", "Alger", "Luanda", "Porto-Novo", "Gaborone", "Ouagadougou", "Bujumbura", "Yaoundé", "Praia", "Bangui", "Moroni", "Brazzaville", "Kinshasa", "Yamoussoukro", "Djibouti", "Le Caire", "Asmara", "Addis-Abeba", "Libreville", "Banjul", "Accra", "Conakry", "Bissau", "Malabo", "Nairobi", "Maseru", "Monrovia", "Tripoli", "Antananarivo", "Lilongwe", "Bamako", "Rabat", "Port-Louis", "Nouakchott", "Maputo", "Windhoek", "Niamey", "Abidjan", "Kampala", "Kigali", "São Tomé", "Dakar", "Victoria", "Freetown", "Mogadiscio", "Khartoum", "Djouba", "Mbabane", "Dodoma", "N'Djaména", "Lomé", "Tunis", "Lusaka", "Harare"];   
     firstgame("Afrique");
 }
 function amerique(){
-    var pays = ["Antigua-et-Barbuda", "Argentine", "Bahamas", "Barbade", "Belize", "Bolivie", "Brésil", "Canada", "Chili", "Colombie", "Costa Rica", "Cuba", "République dominicaine", "Dominique", "Équateur", "États-Unis", "Grenade", "Guatemala", "Guyana", "Haïti", "Honduras", "Jamaïque", "Mexique", "Nicaragua", "Panama", "Paraguay", "Pérou", "Saint-Christophe-et-Niévès", "Saint-Vincent-et-les-Grenadines", "Sainte-Lucie", "Salvador", "Suriname", "Trinité-et-Tobago", "Uruguay", "Venezuela"];
-    var capitales = ["Saint John's", "Buenos Aires", "Nassau", "Bridgetown", "Belmopan", "La Paz", "Brasilia", "Ottawa", "Santiago", "Bogota", "San José", "La Havane", "Saint-Domingue", "Roseau", "Quito", "Washington", "Saint-Georges", "Guatemala", "Georgetown", "Port-au-Prince", "Tegucigalpa", "Kingston", "Mexico", "Managua", "Panama", "Asuncion", "Lima", "Basseterre", "Kingstown", "Castries", "San Salvador", "Paramaribo", "Port-d'Espagne", "Montevideo", "Caracas"];
+    pays = ["Antigua-et-Barbuda", "Argentine", "Bahamas", "Barbade", "Belize", "Bolivie", "Brésil", "Canada", "Chili", "Colombie", "Costa Rica", "Cuba", "République dominicaine", "Dominique", "Équateur", "États-Unis", "Grenade", "Guatemala", "Guyana", "Haïti", "Honduras", "Jamaïque", "Mexique", "Nicaragua", "Panama", "Paraguay", "Pérou", "Saint-Christophe-et-Niévès", "Saint-Vincent-et-les-Grenadines", "Sainte-Lucie", "Salvador", "Suriname", "Trinité-et-Tobago", "Uruguay", "Venezuela"];
+    capitales = ["Saint John's", "Buenos Aires", "Nassau", "Bridgetown", "Belmopan", "La Paz", "Brasilia", "Ottawa", "Santiago", "Bogota", "San José", "La Havane", "Saint-Domingue", "Roseau", "Quito", "Washington", "Saint-Georges", "Guatemala", "Georgetown", "Port-au-Prince", "Tegucigalpa", "Kingston", "Mexico", "Managua", "Panama", "Asuncion", "Lima", "Basseterre", "Kingstown", "Castries", "San Salvador", "Paramaribo", "Port-d'Espagne", "Montevideo", "Caracas"];
     firstgame("Amérique");
 }
 function europe(){
-    var pays = ["Albanie", "Allemagne", "Andorre", "Arménie", "Autriche", "Azerbaïdjan", "Belgique", "Biélorussie", "Bosnie-Herzégovine", "Bulgarie", "Chypre", "Croatie", "Danemark", "Espagne", "Estonie", "Finlande", "France", "Géorgie", "Grèce", "Hongrie", "Irlande", "Islande", "Italie", "Kosovo", "Lettonie", "Liechtenstein", "Lituanie", "Luxembourg", "Macédoine du Nord", "Malte", "Moldavie", "Monaco", "Monténégro", "Norvège", "Pays-Bas", "Pologne", "Portugal", "République tchèque", "Roumanie", "Royaume-Uni", "Russie", "Saint-Marin", "Serbie", "Slovaquie", "Slovénie", "Suède", "Suisse", "Turquie", "Ukraine", "Vatican"];
-    var capitales = ["Tirana", "Berlin", "Andorre-la-Vieille", "Erevan", "Vienne", "Bakou", "Bruxelles", "Minsk", "Sarajevo", "Sofia", "Nicosie", "Zagreb", "Copenhague", "Madrid", "Tallinn", "Helsinki", "Paris", "Tbilissi", "Athènes", "Budapest", "Dublin", "Reykjavik", "Rome", "Pristina", "Riga", "Vaduz", "Vilnius", "Luxembourg", "Skopje", "La Valette", "Chisinau", "Monaco", "Podgorica", "Oslo", "Amsterdam", "Varsovie", "Lisbonne", "Prague", "Bucarest", "Londres", "Moscou", "Saint-Marin", "Belgrade", "Bratislava", "Ljubljana", "Stockholm", "Berne", "Ankara", "Kiev", "Vatican"];
+    pays = ["Albanie", "Allemagne", "Andorre", "Arménie", "Autriche", "Azerbaïdjan", "Belgique", "Biélorussie", "Bosnie-Herzégovine", "Bulgarie", "Chypre", "Croatie", "Danemark", "Espagne", "Estonie", "Finlande", "France", "Géorgie", "Grèce", "Hongrie", "Irlande", "Islande", "Italie", "Kosovo", "Lettonie", "Liechtenstein", "Lituanie", "Luxembourg", "Macédoine du Nord", "Malte", "Moldavie", "Monaco", "Monténégro", "Norvège", "Pays-Bas", "Pologne", "Portugal", "République tchèque", "Roumanie", "Royaume-Uni", "Russie", "Saint-Marin", "Serbie", "Slovaquie", "Slovénie", "Suède", "Suisse", "Turquie", "Ukraine", "Vatican"];
+    capitales = ["Tirana", "Berlin", "Andorre-la-Vieille", "Erevan", "Vienne", "Bakou", "Bruxelles", "Minsk", "Sarajevo", "Sofia", "Nicosie", "Zagreb", "Copenhague", "Madrid", "Tallinn", "Helsinki", "Paris", "Tbilissi", "Athènes", "Budapest", "Dublin", "Reykjavik", "Rome", "Pristina", "Riga", "Vaduz", "Vilnius", "Luxembourg", "Skopje", "La Valette", "Chisinau", "Monaco", "Podgorica", "Oslo", "Amsterdam", "Varsovie", "Lisbonne", "Prague", "Bucarest", "Londres", "Moscou", "Saint-Marin", "Belgrade", "Bratislava", "Ljubljana", "Stockholm", "Berne", "Ankara", "Kiev", "Vatican"];
     firstgame("Europe");
 }
 function oceanie(){
-    var pays = ["Australie", "Fidji", "Kiribati", "Îles Marshall", "Micronésie", "Nauru", "Nouvelle-Zélande", "Palaos", "Papouasie-Nouvelle-Guinée", "Salomon", "Samoa", "Tonga", "Tuvalu", "Vanuatu"];
-    var capitales = ["Canberra", "Suva", "Tarawa-Sud", "Majuro", "Palikir", "Yaren", "Wellington", "Melekeok", "Port Moresby", "Honiara", "Apia", "Nukuʻalofa", "Funafuti", "Port-Vila"];
+    pays = ["Australie", "Fidji", "Kiribati", "Îles Marshall", "Micronésie", "Nauru", "Nouvelle-Zélande", "Palaos", "Papouasie-Nouvelle-Guinée", "Salomon", "Samoa", "Tonga", "Tuvalu", "Vanuatu"];
+    capitales = ["Canberra", "Suva", "Tarawa-Sud", "Majuro", "Palikir", "Yaren", "Wellington", "Melekeok", "Port Moresby", "Honiara", "Apia", "Nukuʻalofa", "Funafuti", "Port-Vila"];
     firstgame("Océanie");
 }
 
@@ -112,4 +132,13 @@ function firstgame(continent){
     document.querySelector(".titre").innerHTML = "Capitales d'" + continent;
 
     jeu();
+}
+
+function indice(){
+    clue = true;
+    //on veut indiquer la première lettre de la capitale dans le champ de réponse
+    var indice = capitales[i].charAt(0);
+    document.querySelector("#reponse").value = indice;
+    document.querySelector("#reponse").focus();
+
 }
